7e0267730993b40275fc4669f70b73780c1d9e44
Jaseunda <76813779+Jaseunda@users.noreply.github.com>
2023-04-08 00:17:30 +0800

    [JohnDave][Update] make some changes on description

--- setup.py
+++ setup.py
@@ -1,18 +1,37 @@
 from setuptools import setup, find_packages
+import os
+
+# Read the contents of your README file
+with open("docs/README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
-    name="reqauto",
-    version="0.1.0",
+    name="reauto",
+    version="0.1.1",
     author="Jaseunda",
-    author_email="support@jaseunda.com",
-    description="A tool to automatically generate and install requirements.txt.",
+    author_email="opensource@jaseunda.com",
+    description="A Python package to automatically generate and install requirements.txt",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/piply",
     packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
     install_requires=[
         "setuptools",
     ],
     entry_points={
         "console_scripts": [
-            "autoreq=autoreq:main",
+            "piply=piply:main",
         ],
     },
+    python_requires=">=3.6",
 )
